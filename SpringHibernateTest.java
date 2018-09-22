a4eb33fe1cd71b4c38a18ffdcfb34b1913451f6b
Tuğcan <tugcankoparan10@gmail.com>
2018-09-22 23:04:27 +0300

    Update SpringHibernateTest.java

--- SpringHibernateTest.java
+++ SpringHibernateTest.java
@@ -1,45 +1,32 @@
-package erguder.levent._22.PropertyPlaceholderConfigurer;
+package PropertyPlaceholderConfigurer;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringHibernateTest {
 	public static void main(String[] args) {
-		ApplicationContext appContext = new ClassPathXmlApplicationContext("tutorial_22.xml");
+		ApplicationContext appContext = new ClassPathXmlApplicationContext("config.xml");
 
 		EmployeeDao empDao = (EmployeeDao) appContext.getBean("empDaoId");
 
-		// insert record
-		// kayit ekleyelim
-
 		Employee emp = new Employee();
 		emp.setId(1);
-		emp.setName("Levent");
-		emp.setSurname("Erguder");
-		emp.setSalary(10000);
+		emp.setName("Tuğcan");
+		emp.setSurname("Koparan");
+		emp.setSalary(1111);
 		empDao.hire(emp);
-
-		Employee emp2 = new Employee();
-		emp2.setId(2);
-		emp2.setName("Burak");
-		emp2.setSurname("Okumus");
-		emp2.setSalary(20000);
-		empDao.hire(emp2);
-
-		// select
-		// kaydi sorgulayalim
-		Employee emp3 = empDao.findByName("Levent");
+		
+		Employee emp1 = empDao.findByName("Tuğcan");
+		
 		System.out.println(emp3);
 
-		// update
-		// guncelleme islemi yapalim
 	 	emp3.setSalary(15000);
+		
 		empDao.updateInfo(emp3);
+		
 		System.out.println(emp3);
-
-		// delete
-		// silme islemi yapalim
-	   empDao.discharge(emp3);
+		
+	        empDao.discharge(emp3);
 
 	}
 }
